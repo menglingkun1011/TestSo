cf4955315754e342046e3ced4d9106646ee94489
mlk <v_menglingkun01@soimt.com>
2025-06-28 17:15:02 +0800

    添加intArrFromJNI方法

--- src/native-lib.cpp
+++ src/native-lib.cpp
@@ -6,11 +6,33 @@
 // src/native-lib.cpp
 #include <jni.h>
 #include <string>
+#include <android/log.h>
+#define TAG "native-jni"
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR,TAG,__VA_ARGS__)
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO,TAG,__VA_ARGS__)
+
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_example_mirsmeng_ndk01_JniUtils_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
     std::string hello = "Hello from C++, from java JniUtils file";
+    jintArray pArray = env->NewIntArray(2);
     return env->NewStringUTF(hello.c_str());
+}
+
+extern "C" JNIEXPORT void JNICALL
+Java_com_example_mirsmeng_ndk01_JniUtils_intArrFromJNI(
+        JNIEnv* env,
+        jobject /* this */,
+        jintArray jintArr) {
+//    jintArray pArray = env->NewIntArray(2);
+    jint len = 10;
+    jint nativeArr[len];
+    env->GetIntArrayRegion(jintArr ,0,len,nativeArr);
+    for (int i = 0; i < len; ++i) {
+        nativeArr[i] = i + 10;
+        LOGI("nativeArr[%d]:%d",i,nativeArr[i]);
+    }
+    env->SetIntArrayRegion(jintArr,0,len,nativeArr);
 }
